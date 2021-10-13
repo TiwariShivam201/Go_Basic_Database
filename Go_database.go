aff0eba1088914d44fb5ec14c6cac13584e69f85
Shivam <s1032170105@gmail.com>
2021-10-13 12:37:10 +0530

    First Commit

---
+++ Go_database.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Students struct {
+	ID      int
+	Name    string
+	Age     int
+	Address string
+}
+
+func main() {
+	// Connecting Database using Go Lang
+	db, err := sql.Open("mysql", "root:Sal@12345678@tcp(127.0.0.1:3306)/student")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	// Insert data into table
+	/*insert, err := db.Query("INSERT INTO students(ID,Name,Age,Address) VALUES('11','Sameer','30','Mumbai')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	insert.Close()*/
+
+	// Get data from database
+	result, err := db.Query("SELECT ID,Name,Age,Address FROM students")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	for result.Next() {
+		var Student Students
+		err := result.Scan(&Student.ID, &Student.Name, &Student.Age, &Student.Address)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(Student)
+	}
+	fmt.Println("Hello World")
+}
